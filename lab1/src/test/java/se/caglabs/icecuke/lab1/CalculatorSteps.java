349a777db4331e3c2248f288e245dd2ee4f0977d
Joacim Breiler <joacim@nimlab.se>
2016-03-09 22:14:10 +0100

    Skapade ett utkast till miniräknare

--- lab1/src/test/java/se/caglabs/icecuke/lab1/CalculatorSteps.java
+++ lab1/src/test/java/se/caglabs/icecuke/lab1/CalculatorSteps.java
@@ -1,25 +1,24 @@
 package se.caglabs.icecuke.lab1;
 
-import cucumber.api.*;
 import cucumber.api.java.sv.*;
+import org.junit.Assert;
 
 public class CalculatorSteps {
+
+    private Calculator calculator;
+
     @Givet("^att kalkylator är startad$")
     public void att_kalkylator_är_startad() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        calculator = new Calculator();
     }
 
     @När("^jag summerar (\\d+) och (\\d+)$")
     public void jag_summerar_och(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        calculator.add(arg1).add(arg2);
     }
 
     @Så("^är summan (\\d+)$")
     public void är_summan(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals(Double.valueOf(arg1), Double.valueOf(calculator.getDisplay()));
     }
-
 }
