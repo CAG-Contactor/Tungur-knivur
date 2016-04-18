bdcc2ab85eeb31f7d35fa5eb422f9d49656d5c07
Joacim Breiler <joacim@nimlab.se>
2016-04-18 20:24:32 +0200

    Ändrade datatypen i kalkylatorn så att den fixar decimaltal (flyttal är inte exakta vilket kan orsaka jobbiga problem i testningen)

--- lab1/src/main/java/se/caglabs/icecuke/lab1/Calculator.java
+++ lab1/src/main/java/se/caglabs/icecuke/lab1/Calculator.java
@@ -1,45 +1,45 @@
 package se.caglabs.icecuke.lab1;
 
+import java.math.*;
 import java.util.*;
 
 public class Calculator {
 
-    private Double display;
+    private BigDecimal display;
 
     public Calculator() {
         clear();
     }
 
     public Calculator clear() {
-        display = 0.0;
+        display = new BigDecimal(0.0);
         return this;
     }
 
-    public Calculator add(double value) {
-        display += value;
+    public Calculator add(BigDecimal value) {
+        display = display.add(value);
         return this;
     }
 
-    public Calculator subtract(double value) {
-        display -= value;
+    public Calculator subtract(BigDecimal value) {
+        display = display.subtract(value);
         return this;
     }
 
-    public Calculator multiply(double value) {
-        display *= value;
+    public Calculator multiply(BigDecimal value) {
+        display = display.multiply(value);
         return this;
     }
 
-    public double getDisplay() {
+    public BigDecimal getDisplay() {
         return display;
     }
 
-    public Calculator average(Double... values) {
+    public Calculator average(List<BigDecimal> values) {
         // Recalculate the current average
-        display = Arrays.asList(values)
-                .stream()
-                .mapToDouble(Double::doubleValue)
-                .sum() / values.length;
+        display = values.stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .divide(BigDecimal.valueOf(values.size()), 5, BigDecimal.ROUND_HALF_UP);
         return this;
     }
 }
