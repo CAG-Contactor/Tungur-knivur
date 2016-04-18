21a19f50fdbcd76fc222725d15cb8511943426cd
Joacim Breiler <joacim@nimlab.se>
2016-04-18 18:58:12 +0200

    Lade till några fler tester i facit samt ändrade datatypen i kalkylatorn så att den fixar decimaltal

--- lab1/features/en_egenskap.feature
+++ lab1/features/en_egenskap.feature
@@ -1,3 +1,35 @@
 # language: sv
 Egenskap: En egenskap
 
+  Scenario: Det ska gå att summera och subtrahera
+    Givet att min kalkylator är nollställd
+    När jag matar in plus 10
+    När jag matar in minus 4
+    Så har jag 6
+
+  Scenario: Det ska gå att summera två tal
+    Givet att min kalkylator är nollställd
+    När jag summerar 1,3 och 2,1
+    Så har jag 3,4
+
+  Scenariomall: Alla räknesätt ska gå att kombinera
+    Givet att min kalkylator är nollställd
+    När jag matar in plus <plus>
+    När jag matar in minus <minus>
+    När jag multiplicerar med <multiplicera>
+    Så har jag <summa>
+
+    Exempel: Exempel på räknesätt
+      | plus | minus | multiplicera | summa |
+      | 1    | 1     | 1            | 0     |
+      | 0    | 1     | 1            | -1    |
+      | 0    | 0     | 1            | 0     |
+
+  Scenario: Det ska gå att räkna ut ett medelvärde
+    Givet att min kalkylator är nollställd
+    När jag matar in dessa tal för att räkna ut medeltal
+      | 10 |
+      | 20 |
+      | 10 |
+      | 15 |
+    Så har jag 13,75
--- lab1/src/test/java/se/caglabs/icecuke/lab1/CalculatorSteps.java
+++ lab1/src/test/java/se/caglabs/icecuke/lab1/CalculatorSteps.java
@@ -1,24 +1,49 @@
 package se.caglabs.icecuke.lab1;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.sv.*;
 import org.junit.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public class CalculatorSteps {
 
     private Calculator calculator;
 
-    @Givet("^att kalkylator är startad$")
+    @Givet("^att min kalkylator är nollställd$")
     public void att_kalkylator_är_startad() throws Throwable {
         calculator = new Calculator();
     }
 
-    @När("^jag summerar (\\d+) och (\\d+)$")
-    public void jag_summerar_och(int arg1, int arg2) throws Throwable {
+    @När("^jag summerar (.*) och (.*)$")
+    public void jag_summerar_och(BigDecimal arg1, BigDecimal arg2) throws Throwable {
         calculator.add(arg1).add(arg2);
     }
 
-    @Så("^är summan (\\d+)$")
-    public void är_summan(int arg1) throws Throwable {
-        Assert.assertEquals(Double.valueOf(arg1), Double.valueOf(calculator.getDisplay()));
+    @Så("^har jag (.*)$")
+    public void är_summan(BigDecimal arg1) throws Throwable {
+        Assert.assertTrue("Förväntade " + calculator.getDisplay() + " men var " + arg1, calculator.getDisplay().compareTo(arg1) == 0);
+    }
+
+    @När("^jag matar in plus (.*)$")
+    public void jag_matar_in_plus(BigDecimal arg1) throws Throwable {
+        calculator.add(arg1);
+    }
+
+    @När("^jag matar in minus (.*)$")
+    public void jag_matar_in_minus(BigDecimal arg1) throws Throwable {
+        calculator.subtract(arg1);
+    }
+
+    @När("^jag multiplicerar med (.*)$")
+    public void jag_multiplicerar_med_multiplicera(BigDecimal arg1) throws Throwable {
+        calculator.multiply(arg1);
+    }
+
+    @När("^jag matar in dessa tal för att räkna ut medeltal$")
+    public void jag_matar_in_dessa_tal_för_att_räkna_ut_medeltal(DataTable arg1) throws Throwable {
+        List<BigDecimal> tal = arg1.asList(BigDecimal.class);
+        calculator.average(tal);
     }
 }
